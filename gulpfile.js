573d49182362935d9dc883cf1063f805be9867cf
jecyu <jecyu.lin@gmail.com>
2024-05-09 07:43:16 +0800

    feat: 添加单元测试

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const babel = require("gulp-babel");
 const less = require("gulp-less");
 const autoprefixer = require("gulp-autoprefixer");
 const cssnano = require("gulp-cssnano");
-const through2 = require('through2')
+const through2 = require("through2");
 const paths = {
   dest: {
     lib: "lib", // commonjs 文件存放的目录名 - 本块关注
@@ -11,7 +11,11 @@ const paths = {
     dist: "dist", // umd文件存放的目录名 - 暂时不关心
   },
   styles: "src/**/*.less", // 样式文件路径 - 暂时不关心
-  scripts: ["src/**/*.{ts,tsx}", "!src/**/demo/*.{ts,tsx}"], // 脚本文件路径
+  scripts: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/demo/*.{ts,tsx}",
+    "!src/**/__tests__/*.{ts,tsx}",
+  ], // 脚本文件路径
 };
 
 /**
